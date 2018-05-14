771e15cfadcb3b005ac7c045004593ef72e3350e
pbound <mitrkaruna@gmail.com>
2018-05-14 18:19:33 +0700

    add ok.ru
    ad serve ok.ru

--- plugin.video.kseries/default.py
+++ plugin.video.kseries/default.py
@@ -11,7 +11,7 @@ def get_categories():
     for ctg in catgList:
         plugintools.add_item(title=ctg.get('title'),action='showseries',url=ctg.get('url'))
     plugintools.close_item_list()
-    
+
 def get_shows(url):
     showsList = asian4HB.getSeries(url)
     for show in showsList:
@@ -23,7 +23,7 @@ def get_shows(url):
         else:
             plugintools.add_item(title=show.get('title'),action='showseries',url=show.get('url'),thumbnail=thumb)
     plugintools.close_item_list()
-    
+
 def get_episodes(url,thumbnail):
     try:
         epsList = asian4HB.getEpisodes(url)
@@ -42,16 +42,18 @@ def get_streams(url,title,thumbnail):
     for stream in strmList:
         plugintools.add_item(title=stream.get('title'),action='stream',url=stream.get('url'),thumbnail=thumbnail)
     plugintools.close_item_list()
-    
+
 def stream(url,title,thumbnail):
     if 'google' in url:
+        path = urlresolver.HostedMediaFile(url).resolve()
+    elif 'ok.ru' in url:
         path = urlresolver.HostedMediaFile(url).resolve()
     else:
         path = url
     li = xbmcgui.ListItem(label=title, iconImage=thumbnail, thumbnailImage=thumbnail,path=path)
     li.setInfo(type='Video', infoLabels={ "Title": str(title) })
     xbmc.Player().play(path,li)
-    
+
 def run():
     params = plugintools.get_params()
     action = params.get('action')
@@ -65,6 +67,6 @@ def run():
         get_streams(params.get('url'),params.get('title'),params.get('thumbnail'))
     elif action == 'stream':
         stream(urllib.unquote_plus(params.get('url')),params.get('title'),params.get('thumbnail'))
-    
+
 run()
 # print get_categories()
