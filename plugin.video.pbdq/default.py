0108333ffb49c8e143785aff89d51eb7bfc6aeae
pbound <mitrkaruna@gmail.com>
2018-06-08 21:35:26 +0700

    edit code to v2
    newcode

--- plugin.video.pbdq/default.py
+++ plugin.video.pbdq/default.py
@@ -3,30 +3,17 @@ import os
 import sys
 import urllib2
 
-import urlresolver,plugintools,urllib,xbmcplugin,xbmc,xbmcgui,re,requests,yaddon
+import urlresolver,plugintools,urllib,xbmcplugin,xbmc,xbmcgui,re,requests,nim
 from bs4 import BeautifulSoup
 #down.upf.co.il/downloadnew
-def upf(url):
-    s = requests.Session()
-    s.headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.106 Safari/537.36'}
-    r=s.get(url)
-    soup = BeautifulSoup(r.text,"html5lib")
-    hidden = soup.find("input",{"type":"hidden"}).get("value")
-    print url.replace("www.upf.co.il","down.upf.co.il/downloadnew").replace(".html","/")+hidden
-    return url.replace("www.upf.co.il","down.upf.co.il/downloadnew").replace(".html","/")+hidden
 
 
-def get_categories():
-    catgList = yaddon.getMenu()
-    for ctg in catgList:
-        plugintools.add_item(title=ctg.get('title'),action='showyear',url=ctg.get('url'))
-    plugintools.close_item_list()
 
-def get_gerne(url):
-    gerList = yaddon.getgerne(url)
+def get_gerne():
+    gerList = nim.getgenre()
     for ger in gerList:
-        plugintools.add_item(title=ger.get('title'),action='showyear',url=ger.get('url'),gid=ger.get('gid'))
+        plugintools.add_item(title=ger.get('title'),action='showseries',url=ger.get('url'))
+    # plugintools.add_item(title=u'ค้นหา ', action='showsearch')
     plugintools.close_item_list()
 
 
@@ -36,22 +23,50 @@ def get_year(url):
         # plugintools.add_item(title=gid,action='showseries',url=year.get('url'),gid=gid)
         plugintools.add_item(title=year.get('title'), action='showseries', url=year.get('url'), yid=year.get('yid'))
     plugintools.close_item_list()
+def get_search():
+    # slist = site2list()
+    # menuItems = slist[1]
+    # selecturl = xbmcgui.Dialog().select('Select Sites', menuItems)
+    qry =plugintools.keyboard_input('','ค้นหา')
 
-    
-def get_shows(url,yid):
-    showsList = yaddon.getSeries(url,yid)
+    # qry = get_keyboard('ค้นหา',default='')
+    # xbmcgui.Dialog().ok('test', str(slist[0][selecturl]))
+    # siteslist =  getsiteslist()
+
+
+    # if select == -1:
+    #     return None
+    #     break
+    # else:
+    #     return menuItems[select]
+    # strmList = importsite(url, 'getstreams')
+
+    # surl = slist[0][selecturl]
+    showsList = nim.getsearch(qry)
     for show in showsList:
         thumb = show.get('thumbnail')
         if thumb == None:
             thumb = ""
-        if show.get('title')!= u"โหลดทังหมด" :
+        if show.get('title') != u"Next":
+            plugintools.add_item(title=show.get('title'), action='showepisodes', url=show.get('url'), thumbnail=thumb)
+        else:
+            plugintools.add_item(title=show.get('title'), action='showmovie', url=show.get('url'), thumbnail=thumb)
+    plugintools.close_item_list()
+
+def get_shows(url):
+    showsList = nim.getseries(url)
+    for show in showsList:
+        thumb = show.get('thumbnail')
+        if thumb == None:
+            thumb = ""
+        if show.get('title')!= u"Next" :
             plugintools.add_item(title=show.get('title'),action='showepisodes',url=show.get('url'),thumbnail=thumb)
         else:
-            plugintools.add_item(title=show.get('title'),action='showseriesall',url=urllib.unquote(url).decode('utf8'),thumbnail=thumb,yid=yid)
+            plugintools.add_item(title=show.get('title'),action='showseries',url=urllib.unquote(url).decode('utf8'),thumbnail=thumb)
     plugintools.close_item_list()
 
 
-def get_showsall(url, yid):
+def get_showsall(url):
     showsList = yaddon.getSeriesAll(url, yid)
     for show in showsList:
         thumb = show.get('thumbnail')
@@ -65,85 +80,30 @@ def get_showsall(url, yid):
 
 
 def get_episodes(url):
-    epsList = yaddon.getEpisodes(url)
-    # epid = yaddon.getEpisodes('epid')
-    for episode in epsList:
-        epid = episode.get('epid')
-        thumb = episode.get('thumbnail')
-        plugintools.add_item(title=episode.get('title'),action='streamslist',url=episode.get('url'),thumbnail=thumb,epid=epid)
+    epsList = nim.getepisode(url)
+    for show in epsList:
+        # print show
+        thumb = show.get('thumbnail')
+        if thumb == None:
+            thumb = ""
+        plugintools.add_item(title=show.get('title'),action='streamslist',url=show.get('url'),thumbnail=thumb)
+
     plugintools.close_item_list()
 
-    
 
-def get_streams(url,epid):
-    strmList = yaddon.getStreams(url,epid)
+
+def get_streams(url, title, thumbnail):
+    strmList = nim.getstreams(url)
     # epid = yaddon.getStreams('epid')
-
     for stream in strmList:
-        sub = stream.get('epid')
-        plugintools.add_item(title=stream.get('title'),action='stream',url=stream.get('url'),epid=sub)
-        # plugintools.add_item(title=sub, action='stream', url=stream.get('url'), epid=sub)
+        stitle = title.decode('utf8') + ' ' + stream.get('title')
+        plugintools.add_item(title=stitle, action='stream', url=stream.get('url'),
+                             thumbnail=urllib.unquote(thumbnail).decode('utf8'))
     plugintools.close_item_list()
-    # Download_subtitle(sys.path[0] + '/resources/temp', 'nameteat', sub)
-
-def Download_subtitle(subs, videoTitle, url):
-    if not os.path.exists(subs):
-        os.makedirs(subs)
-    filename = str(videoTitle) + '.srt'
-    filepath = xbmc.translatePath(os.path.join(subs, filename))
-
-    def download(url, dest):
-        pip = '127.0.0.1:3128'
-        proxy_handler = urllib2.ProxyHandler({'http': pip})
-        opener = urllib2.build_opener(proxy_handler)
-        opener.addheaders = [('User-agent', 'Mozilla/5.0')]
-        urllib2.install_opener(opener)
-        req = urllib2.Request(url)
-        sock = urllib2.urlopen(req)
-
-        fi = open(dest, "w")
-        fi.write(sock.read())
-        fi.close()
-        # proxy = urllib.request.ProxyHandler({'http': '127.0.0.1:3128'})
-        # construct a new opener using your proxy settings
-        # opener = urllib.request.build_opener(proxy)
-        # install the openen on the module-level
-        # urllib.request.install_opener(opener)
-        ##                    dialog = xbmcgui.DialogProgress()
-        ##                    dialog.create('Downloading Movie','From Source', filename)
-        # urllib.urlretrieve(url, dest, lambda nb, bs, fs, url=url: _pbhook(nb, bs, fs, url, ''))
-
-    def _pbhook(numblocks, blocksize, filesize, url=None, dialog=None):
-        try:
-
-            percent = min((numblocks * blocksize * 100) / filesize, 100)
-        ##                        dialog.update(percent)
-        except:
-            percent = 100
-            ##                        dialog.update(percent)
-            ##                    if dialog.iscanceled():
-            ##                                    dialog.close()
-
-    download(url, filepath)
-    iscanceled = True
-    xbmc.executebuiltin('Notification("Subtitle","Downloaded")')
-    return filepath
 
 
-def stream(url, title, thumbnail,epid):
-    # if 'upf' in url:
-    #     resolved_url = upf(url.replace("upfile",'upf'))
-    # elif "yadi" in url:
-    #     resolved_url = asian4HB.yandex(url)
-    # else:
-    #     final=urlresolver.HostedMediaFile(url)
-    #     new_url=final.get_url()
-    #     resolved_url=urlresolver.resolve(new_url)
-    # path=resolved_url
+def stream(url, title, thumbnail):
     path = url
-    # subtitle_url =  str(epid).replace("u'",'')
-    # subtitle_url = 'https://www.grabvdo.com/subtitle?viu=eyJpdiI6IlBSWUV4M2tIODdjelwvK09GNWFlcWNBPT0iLCJ2YWx1ZSI6Ino3bENuVXVkQ0M1OUNwdDJHZnBQUm9ZZEd1b3BrVXNaVk1SMldacFlhM3lEZDcyMyszM0VVMmViTlwvTVQySUFXemlUdWV1eEJwRjVaa3daR1JiVDRLWjVRWjRib0V0SVE0a0xmVTZiMUdKRm1JejBkbTU1QzJVREJkMVNDdXlMTTB6S05RTTFOa2trUk02Z3hveStWbGc9PSIsIm1hYyI6ImFlNzJlMTIzMGQxNDc5YmQzMjUxYWY3OGFmYmQwMzc5YmFiNzgxY2U4YmJlZjNhZjlmZTg3MzhiYWU0YjljZTEifQ=='
-    # subtitle = Download_subtitle(sys.path[0] + '/resources/temp', 'nameteat', subtitle_url )
     li = xbmcgui.ListItem(label=title, iconImage=thumbnail, thumbnailImage=thumbnail, path=path)
     li.setInfo(type='Video', infoLabels={"Title": str(title)})
     li.setSubtitles([sys.path[0] + '/resources/temp/s.srt'])
@@ -154,20 +114,20 @@ def run():
     params = plugintools.get_params()
     action = params.get('action')
     if action == None:
-        get_categories()
-    elif action == 'showgerne':
-        get_gerne(params.get('url'))
+        get_gerne()
+    elif action == 'showsearch':
+        get_search()
     elif action == 'showyear':
         get_year(params.get('url'))
     elif action == 'showseries':
-        get_shows(params.get('url'),params.get('yid'))
+        get_shows(params.get('url'))
     elif action == 'showseriesall':
-        get_showsall(params.get('url'),params.get('yid'))
+        get_showsall(params.get('url'))
     elif action == 'showepisodes':
         get_episodes(params.get('url'))
     elif action == 'streamslist':
-        get_streams(params.get('url'),params.get('epid'))
+        get_streams(params.get('url'), params.get('title'), params.get('thumbnail'))
     elif action == 'stream':
-        stream(urllib.unquote_plus(params.get('url')),params.get('title'),params.get('thumbnail'),params.get('epid'))
-    
+        stream(urllib.unquote_plus(params.get('url')),params.get('title'),params.get('thumbnail'))
+
 run()
---
+++ plugin.video.pbdq/nim.py
@@ -0,0 +1,176 @@
+# -*- coding: utf-8 -*-
+import json
+
+import os
+import requests
+import sys
+import xbmcgui
+from bs4 import BeautifulSoup
+from base64 import b64decode
+
+baseurl = b64decode('aHR0cHM6Ly93d3cudml1LmNvbS9vdHQvdGgvaW5kZXgucGhw')
+# fbaseurl =b64decode('aHR0cHM6Ly9kZnA2cmdsZ2pxc3prLmNsb3VkZnJvbnQubmV0')
+fbaseurl = b64decode('aHR0cHM6Ly9kZnA2cmdsZ2pxc3prLmNsb3VkZnJvbnQubmV0L2luZGV4LnBocD9yPXYxL3NlYXJjaC9wcmVkaWN0aW9uJnBsYXRmb3JtX2ZsYWdfbGFiZWw9d2ViJmFyZWFfaWQ9NCZsYW5ndWFnZV9mbGFnX2lkPTQma2V5d29yZD0=')
+stmbaseurl = b64decode('aHR0cHM6Ly9kMWsydXM2NzFxY29hdS5jbG91ZGZyb250Lm5ldC9kaXN0cmlidXRlX3dlYl90aC5waHA/Y2NzX3Byb2R1Y3RfaWQ9')
+caturl = baseurl + '?r=category/series-category&platform_flag_label=web&area_id=4&language_flag_id=4&'
+# surl = hurl + 'category_id=' + cateid + '&length=14'
+def getgenre():
+    r = requests.get(baseurl)
+    r.encoding = "utf-8"
+    soup = BeautifulSoup(r.text, 'html5lib')
+    soup.prettify()
+    ul = soup.find('ul', {'class': "v-nav test"})
+    li = ul.findAll('a', {'class': ''})
+    seriesList = []
+    # print (li)
+    for link in li:
+        catid = link.get('data-id')
+        surl = caturl+'category_id=' + str(catid) + '&length=14'
+        # print link.text.replace('\n', '')
+        # print link.get('href')
+        seriesList.append({'title': link.text.replace('\n', ''), 'url': surl})
+
+    return seriesList
+
+
+
+def getseries(url):
+    response = requests.get(url)
+    todos = json.loads(response.text)
+    # print todos == response.json()
+    print len(todos)
+    # dictodo = {}
+    data = todos['data']['series']
+    total = todos['data']['category_series_total'][0]['series_total']
+    print 'series total = ' + total
+    print len(data)
+    # print data
+    seriesList = []
+    for serie in data:
+        # print serie.keys()
+        # print serie['name']
+        # print serie['series_id']
+        # print serie['product_id']
+        # print serie['product_image_url']
+        # print serie['cover_image_url']
+        # print serie['category_cover_image_url']
+        seurl = baseurl + '?r=vod/ajax-detail&platform_flag_label=web&area_id=4&language_flag_id=4&product_id='+str(serie['product_id'])
+        # seurl = stmbaseurl +str(serie['product_id'])
+        seriesList.append({'title':serie['name'], 'url': seurl,
+                           'thumbnail': serie['cover_image_url']})
+
+    next = getnext(url)
+    if next != None:
+        nurl = url + '&offset='+str(next)
+        seriesList.append({'title': u"Next", 'url': nurl})
+    return seriesList
+
+def getepisode(url):
+    # print url
+    epurl = url[:-5]
+    # print epurl
+    response = requests.get(url)
+    jdata = json.loads(response.text)
+    # print todos == response.json()
+    jseries = jdata['data']['series']
+    episodes = jseries ['product']
+    # print type(episodes)
+    eplist = []
+    for ep in episodes:
+        # print ep['number']
+        # print ep['synopsis']
+        # print ep['product_id']
+        # print ep['cover_image_url']
+        url = epurl+ep['product_id']
+        eplist.append({'title': ep['number']+'.'+ep['synopsis'] , 'url': url,
+                           'thumbnail': ep['cover_image_url']})
+    return eplist
+def download_subtitle(url):
+    r = requests.get(url)
+    dest = sys.path[0] + '/resources/temp/s.srt'
+    open(dest, 'wb').write(r.content)
+
+def getquality(url):
+    response = requests.get(url)
+    jdata = json.loads(response.text)
+    qualist = jdata['data']['stream']['url']
+    # print type(qualist)
+    sourcelist = []
+    for key in qualist:
+        # print key
+        # print qualist[key]
+        sourcelist.append({"title": key[1:], "url": qualist[key]})
+
+    return sourcelist
+
+
+
+
+
+
+def getstreams(url,title=None):
+    response = requests.get(url)
+    jdata = json.loads(response.text)
+    # print todos == response.json()
+    curproduct = jdata['data']['current_product']
+    # print len(curproduct)
+    sub = curproduct['subtitle']
+    print len(sub)
+    if sub != []:
+        sumsub = len(sub)
+
+        if len(sub)>1:
+            thsubtitle = sub[3]
+        elif len(sub)== 1:
+            thsubtitle = sub[0]
+        subtitleurl = thsubtitle['url']
+        download_subtitle(subtitleurl)
+    else:
+        del_sub()
+        # os.remove("/resources/temp/s.srt")
+
+    ccsproid = curproduct['ccs_product_id']
+    # print 'ccs_product_id= ' + ccsproid
+    qurl = stmbaseurl + ccsproid
+
+    return getquality(qurl)
+
+def getnext(url):
+    offset = url.split('&')
+    total = '233'
+    length = 12
+    # offset = 26
+    # offset = offset+length
+    if 'offset' in  offset[-1]:
+        offsetnum = offset[-1].split('=')[-1]
+        offsetnum = int(offsetnum)+12
+
+    else:
+        offsetnum = 26
+    print offsetnum
+
+    if offsetnum <= 233:
+        return offsetnum
+
+def del_sub():
+    try:
+        os.remove(sys.path[0] + '/resources/temp/s.srt')
+    except OSError:
+        None
+
+def getsearch(arg):
+    url = fbaseurl + arg
+    source = requests.get(url).json()
+
+    sresult = source['data']['series']
+    # print sresult
+    sourcelist = []
+    if sresult !=[]:
+        for result in sresult:
+            # print result['id']
+            # for key in result():
+            #     print key
+            # print sr['id'], sr['name']
+            sourcelist.append({"title": result['name'], "url": stmbaseurl+result['id']})
+
+    return sourcelist
